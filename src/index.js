9ece0463d2609d8d014649ef1937e14e3455f3be
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-02 15:56:27 -0400

    :construction: work

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import { runCode } from './exercise';
+import "./styles.css";
+import { runCode } from "./exercise";
 
-const arrayA = [1, 2, 3, 4];
-const arrayB = [5, 6, 7, 8];
-const rta = runCode(arrayA, arrayB);
+(() => {
+  const arrayA = [1, 2, 3, 4];
+  const arrayB = [5, 6, 7, 8];
+  const rta = runCode(arrayA, arrayB);
 
-document.getElementById("app").innerHTML = `
-<p>Array A: ${arrayA}</p>
-<p>Array B: ${arrayB}</p>
-<p>Rta: ${rta}</p>`;
+  document.getElementById("app").innerHTML = `
+  <h1>Title</h1>
+  <p>Array A: <code>${arrayA}</code></p>
+  <p>Array B: <code>${arrayB}</code></p>
+  <p>Response from <code>runCode</code>
+  <pre><code>${rta}</code></pre>
+  `;
+})();
