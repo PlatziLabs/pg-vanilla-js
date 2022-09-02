40cedd8ac2a11e67e9295f88042cf2afb0b2b8f7
Nicolas Molina Monroy <hi@nicobytes.com>
2022-09-02 16:04:53 -0400

    :construction: work

--- src/exercise.test.js
+++ src/exercise.test.js
@@ -1,15 +1,17 @@
-import { runCode } from './exercise';
+import { runCode } from "./exercise";
 
-it("should return [1,2,3,4]", () => {
-  const arrayA = [1, 2];
-  const arrayB = [3, 4];
-  const rta = runCode(arrayA, arrayB);
-  expect(rta).toEqual([1, 2, 3, 4]);
-});
+describe("tests", () => {
+  it("should return [1,2,3,4]", () => {
+    const arrayA = [1, 2];
+    const arrayB = [3, 4];
+    const rta = runCode(arrayA, arrayB);
+    expect(rta).toEqual([1, 2, 3, 4]);
+  });
 
-it("should return [1,2,3,4,5]", () => {
-  const arrayA = [1, 2];
-  const arrayB = [3, 4, 5];
-  const rta = runCode(arrayA, arrayB);
-  expect(rta).toEqual([1, 2, 3, 4, 5]);
+  it("should return [1,2,3,4,5]", () => {
+    const arrayA = [1, 2];
+    const arrayB = [3, 4, 5];
+    const rta = runCode(arrayA, arrayB);
+    expect(rta).toEqual([1, 2, 3, 4, 5]);
+  });
 });
